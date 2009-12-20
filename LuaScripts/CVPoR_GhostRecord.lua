b5b4a7e1500ee6ce3dea32ba7cd6566c75dae8ae
gochaism <gochaism@d28845b6-a30c-11de-be8d-735280eae207>
2009-12-20 01:21:22 +0000

    add jo/ch hitbox param to ghost dump, and small updates.

--- LuaScripts/CVPoR_GhostRecord.lua
+++ LuaScripts/CVPoR_GhostRecord.lua
@@ -1,7 +1,8 @@
 -- Castlevania - Portrait of Ruin
 -- Ghost record script, based on amaurea's one.
 
-outprefix = "ghost"
+root_dir = ""
+outprefix = root_dir.."ghost"
 dumpfile = outprefix..".dump"
 nomovie = not false
 io.output(dumpfile)
@@ -14,20 +15,24 @@ emu.registerafter(function()
 		world = memory.readbyte(0x02111785)
 		roomx, roomy = memory.readbyte(0x02111778), memory.readbyte(0x0211177a)
 		camx, camy = memory.readdword(0x02111a08), memory.readdword(0x02111a0c)
-		jo_posx, jo_posy = memory.readdword(0x020fcba4), memory.readdword(0x020fcba8)
+		jo_posx, jo_posy = memory.readdwordsigned(0x020fcab0), memory.readdwordsigned(0x020fcab4) -- memory.readdwordsigned(0x020fcba4), memory.readdwordsigned(0x020fcba8)
+		jo_hitx1, jo_hity1 = memory.readwordsigned(0x0213296e), memory.readwordsigned(0x02132970)
+		jo_hitx2, jo_hity2 = memory.readwordsigned(0x02132972), memory.readwordsigned(0x02132974)
 		jo_dir = memory.readbytesigned(0x020ff174)
 		jo_pose = memory.readword(0x020fcb04)
 		jo_blink = memory.readbyte(0x020fca9f)
 		jo_visual = memory.readbyte(0x020fcaf5)
-		ch_posx, ch_posy = memory.readdword(0x020fcd04), memory.readdword(0x020fcd08)
+		ch_posx, ch_posy = memory.readdwordsigned(0x020fcc10), memory.readdwordsigned(0x020fcc14) -- memory.readdwordsigned(0x020fcd04), memory.readdwordsigned(0x020fcd08)
+		ch_hitx1, ch_hity1 = memory.readwordsigned(0x02132982), memory.readwordsigned(0x02132984)
+		ch_hitx2, ch_hity2 = memory.readwordsigned(0x02132986), memory.readwordsigned(0x02132988)
 		ch_dir = memory.readbytesigned(0x020ffdd4)
 		ch_pose = memory.readword(0x020fcc64)
-		ch_visual = memory.readbyte(0x020fcc55)
 		ch_blink = memory.readbyte(0x020fcbff)
-		io.write(string.format("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n",
+		ch_visual = memory.readbyte(0x020fcc55)
+		io.write(string.format("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n",
 			igframe, mode, fade, world, roomx, roomy, camx, camy,
-			jo_posx, jo_posy, jo_dir, jo_pose, jo_blink, jo_visual,
-			ch_posx, ch_posy, ch_dir, ch_pose, ch_blink, ch_visual))
+			jo_posx, jo_posy, jo_hitx1, jo_hity1, jo_hitx2, jo_hity2, jo_dir, jo_pose, jo_blink, jo_visual,
+			ch_posx, ch_posy, ch_hitx1, ch_hity1, ch_hitx2, ch_hity2, ch_dir, ch_pose, ch_blink, ch_visual))
 	end
 end)
 
--- LuaScripts/CVPoR_GhostReplayAviUtl.lua
+++ LuaScripts/CVPoR_GhostReplayAviUtl.lua
@@ -4,9 +4,19 @@
 require("gd")
 require("bit")
 
+copytable = function(t)
+	if t == nil then return nil end
+	local c = {}
+	for k,v in pairs(t) do
+		c[k] = v
+	end
+	setmetatable(c,debug.getmetatable(t))
+	return c
+end
+
 root_dir = ""
-ghost_files = { "HAHA.ghost", "1.ghost", "2.ghost" }
-ghost_trans = { 0, 0.75, 0.75 }
+ghost_files = { "0.ghost", "1.ghost" }
+ghost_trans = { 0, 0.75 }
 jo_gdpose, ch_gdpose = {}, {}
 jo_gdpose.l = gd.createFromPng(root_dir.."jonadb.png"):gdStr()
 jo_gdpose.r = gd.createFromPng(root_dir.."jonadb-r.png"):gdStr()
@@ -30,16 +40,22 @@ for i, v in ipairs(ghost_files) do
 		e.world = io.read("*n")
 		e.roomx = io.read("*n")
 		e.roomy = io.read("*n")
-		e.camx = io.read("*n")
-		e.camy = io.read("*n")
 		e.jo_posx = io.read("*n")
 		e.jo_posy = io.read("*n")
+		e.jo_hitx1 = io.read("*n")
+		e.jo_hity1 = io.read("*n")
+		e.jo_hitx2 = io.read("*n")
+		e.jo_hity2 = io.read("*n")
 		e.jo_dir = io.read("*n")
 		e.jo_pose = io.read("*n")
 		e.jo_blink = (io.read("*n")~=0)
 		e.jo_visual = io.read("*n")
 		e.ch_posx = io.read("*n")
 		e.ch_posy = io.read("*n")
+		e.ch_hitx1 = io.read("*n")
+		e.ch_hity1 = io.read("*n")
+		e.ch_hitx2 = io.read("*n")
+		e.ch_hity2 = io.read("*n")
 		e.ch_dir = io.read("*n")
 		e.ch_pose = io.read("*n")
 		e.ch_blink = (io.read("*n")~=0)
@@ -60,7 +76,7 @@ function func_proc()
 	if f > #ghost[1].data then
 		return
 	end
-	local this = ghost[1].data[f]
+	local this = copytable(ghost[1].data[f])
 	this.fade = math.abs(this.fade)
 	if this.fade > 16 then this.fade = 16 end
 	this.fade = (16 - this.fade) / 16.0
@@ -107,70 +123,3 @@ end
 
 function func_exit()
 end
-
---[[
-emu.registerafter(function()
-	for i = 1, #ghost do
-		if ghost[i].offset <= #ghost[i].data then
-			ghost[i].offset = ghost[i].offset + 1
-		end
-	end
-end)
-
-gui.register(function()
-	local e = {}
-	e.igframe = memory.readdword(0x021119e0)
-	e.mode = memory.readbyte(0x020f6284)
-	e.fade = memory.readbytesigned(0x020f61fc)
-	e.world = memory.readbyte(0x02111785)
-	e.roomx, e.roomy = memory.readbyte(0x02111778), memory.readbyte(0x0211177a)
-	e.camx, e.camy = memory.readdword(0x02111a08), memory.readdword(0x02111a0c)
-	e.jo_posx, e.jo_posy = memory.readdword(0x020fcba4), memory.readdword(0x020fcba8)
-	e.jo_dir = memory.readbytesigned(0x020ff174)
-	e.jo_pose = memory.readword(0x020fcb04)
-	e.jo_blink = (memory.readbyte(0x020fca9f)~=0)
-	e.jo_visual = memory.readbyte(0x020fcaf5)
-	e.ch_posx, e.ch_posy = memory.readdword(0x020fcd04), memory.readdword(0x020fcd08)
-	e.ch_dir = memory.readbytesigned(0x020ffdd4)
-	e.ch_pose = memory.readword(0x020fcc64)
-	e.ch_visual = memory.readbyte(0x020fcc55)
-	e.ch_blink = (memory.readbyte(0x020fcbff)~=0)
-
-
-	local this = e
-
-	clientToScreen = function(x, y)
-		return math.floor((x - this.camx) / 0x1000), math.floor((y - this.camy) / 0x1000)
-	end
-
-	local drawpose = function(gdpose, x, y, n, reverse, opacity)
-		if opacity == nil then opacity = 1.0 end
-		local xi, yi = (n % 0x10), math.floor(n / 0x10)
-		if not reverse then
-			gui.gdoverlay(x - 32, y - 56, gdpose.l, xi * 64, yi * 64, 64, 64, opacity)
-		else
-			gui.gdoverlay(x - 32, y - 56, gdpose.r, (15 - xi) * 64, yi * 64, 64, 64, opacity)
-		end
-	end
-
-	if this.mode ~= 2 or this.fade == 0 then
-		return
-	end
-
-	for i = 1, #ghost do
-		if ghost[i].offset <= #ghost[i].data then
-			local e = ghost[i].data[ghost[i].offset]
-			if e.mode == 2 and (this.world == e.world and this.roomx == e.roomx and this.roomy == e.roomy) then
-				local jo_posx, jo_posy = clientToScreen(e.jo_posx, e.jo_posy)
-				local ch_posx, ch_posy = clientToScreen(e.ch_posx, e.ch_posy)
-				if e.jo_visible then
-					drawpose(jo_gdpose, jo_posx, jo_posy, e.jo_pose, e.jo_dir >= 0, ghost_trans[i] * this.fade * (e.jo_blink and 0.5 or 1.0))
-				end
-				if e.ch_visible then
-					drawpose(ch_gdpose, ch_posx, ch_posy, e.ch_pose, e.ch_dir >= 0, ghost_trans[i] * this.fade * (e.ch_blink and 0.5 or 1.0))
-				end
-			end
-		end
-	end
-end)
-]]
