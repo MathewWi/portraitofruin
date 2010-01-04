f053b7bb97517da6cec06ef62f98228266de747b
gochaism <gochaism@d28845b6-a30c-11de-be8d-735280eae207>
2010-01-04 08:35:23 +0000

    improve simple hud

--- Lua/CVPoR_SimpleHUD.lua
+++ Lua/CVPoR_SimpleHUD.lua
@@ -20,28 +20,33 @@ gui.register(function()
 	local ch_vy = memory.readdwordsigned(0x020fcc20)
 	local ch_inv = memory.readbyte(0x020fcca5)
 	local ch_mptimer = memory.readword(0x020fcbf8)
+	local hp = memory.readword(0x0211216c)
 	local mp = memory.readword(0x02112170)
 	local change_cooldown = memory.readdwordsigned(0x021115fc)
 	local mode = memory.readbyte(0x020f6284)
 	local fade = math.min(1.0, 1.0 - math.abs(memory.readbytesigned(0x020f61fc)/16.0))
 
 	moviemode = movie.mode()
-	if not movie.active() then moviemode = "" end
+	if not movie.active() then moviemode = "no movie" end
+
+	local framestr = ""
+	if movie.active() and not movie.recording() then
+		framestr = string.format("%d/%d", frame, movie.length())
+	else
+		framestr = string.format("%d", frame)
+	end
+	framestr = framestr .. (moviemode ~= "" and string.format(" (%s)", moviemode) or "")
 
 	gui.opacity(opacityMaster)
-	gui.text(1, 26, string.format("%d/%d%s\n%d",
-		frame, igframe,
-		(moviemode ~= "" and string.format(" (%s)", moviemode) or ""),
-		lagframe
-	))
+	gui.text(1, 26, string.format("%s\n%d", framestr, lagframe))
 
 	if mode == 2 then
 		gui.opacity(opacityMaster * (fade/2 + 0.5))
 
-		gui.text(1, 56, string.format("J(%6d,%6d) %d %04X\nC(%6d,%6d) %d %04X\nMP:%3d | %d",
+		gui.text(1, 60, string.format("J(%6d,%6d) %d %04X\nC(%6d,%6d) %d %04X\nHP%03d/MP%03d | %d",
 			jo_vx, jo_vy, jo_inv, jo_mptimer,
 			ch_vx, ch_vy, ch_inv, ch_mptimer,
-			mp, change_cooldown
+			hp, mp, change_cooldown
 		))
 
 		-- enemy info
