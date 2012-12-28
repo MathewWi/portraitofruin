4bb23f0b60ad38a58dcbfd9d49bc89f2f28390c5
gochaism <gochaism@d28845b6-a30c-11de-be8d-735280eae207>
2012-12-28 06:38:23 +0000

    GhostRipHelp: add dirty sprite dump function

--- trunk/Lua/CVPoR_GhostRipHelp.lua
+++ trunk/Lua/CVPoR_GhostRipHelp.lua
@@ -4,7 +4,8 @@ if not bit then require("bit") end
 
 root = ""
 sprw, sprh, sprox, sproy = 128, 128, 64, 120
-jogf, chgf = "richdb.png", "mariadb.png"
+jogf, chgf = "jonadb.png", "chardb.png"
+screenshotKey = "F11"
 
 -- return if an image is a truecolor one
 gd.isTrueColor = function(im)
@@ -35,7 +36,7 @@ gd.convertToTrueColor = function(imsrc)
 	im:alphaBlending(false)
 	local trans = im:colorAllocateAlpha(255, 255, 255, 127)
 	im:filledRectangle(0, 0, im:sizeX() - 1, im:sizeY() - 1, trans)
-	im:copy(imsrc, 0, 0, 0, 0, im:sizeX(), im:sizeY())
+	im:copyResampled(imsrc, 0, 0, 0, 0, im:sizeX(), im:sizeY(), im:sizeX(), im:sizeY())
 	im:alphaBlending(true) -- TODO: set the mode which imsrc uses
 
 	return im
@@ -74,11 +75,78 @@ gd.flipBoth = function(im)
 	gd.flipHorizontal(im)
 	return im
 end
+-- compare two images and extract only different pixels between two images
+-- return value is a truecolor gd image with alpha channel
+gd.createDiff = function(imFG, imBG, ...)
+	local arg = { ... }
+	local xOffsetFG, yOffsetFG = 0, 0
+	local xOffsetBG, yOffsetBG = 0, 0
+	local width, height = math.min(imFG:sizeX(), imBG:sizeX()), math.min(imFG:sizeY(), imBG:sizeY())
 
-jogl = gd.createFromPng(root..jogf):gdStr()
-jogr = gd.flipHorizontal(gd.createFromPng(root..jogf)):gdStr()
-chgl = gd.createFromPng(root..chgf):gdStr()
-chgr = gd.flipHorizontal(gd.createFromPng(root..chgf)):gdStr()
+	-- parse argument array
+	if #arg == 4 then
+		-- xOffset, yOffset, width, height
+		xOffsetFG, yOffsetFG = arg[1], arg[2]
+		xOffsetBG, yOffsetBG = xOffsetFG, yOffsetFG
+		width, height = arg[3], arg[4]
+	elseif #arg == 6 then
+		-- xOffsetFG, yOffsetFG, xOffsetBG, yOffsetBG, width, height
+		xOffsetFG, yOffsetFG = arg[1], arg[2]
+		xOffsetBG, yOffsetBG = arg[3], arg[4]
+		width, height = arg[5], arg[6]
+	elseif #arg > 0 then
+		error("too few/much arguments")
+	end
+
+	-- range check
+	if (xOffsetFG + width > imFG:sizeX()) or (yOffsetFG + height > imFG:sizeY()) then
+		error("foreground image (argument #1) is too small, or illegal offset. " .. width .. "x" .. height .. " from (" .. xOffsetFG .. "," .. yOffsetFG .. ")")
+	end
+	if (xOffsetBG + width > imBG:sizeX()) or (yOffsetBG + height > imBG:sizeY()) then
+		error("background image (argument #2) is too small, or illegal offset. " .. width .. "x" .. height .. " from (" .. xOffsetBG .. "," .. yOffsetBG .. ")")
+	end
+
+	-- create new gd image
+	local imDiff = gd.createTrueColor(width, height)
+	imDiff:alphaBlending(false)
+	imDiff:copyResized(imFG, 0, 0, xOffsetFG, yOffsetFG, width, height, width, height)
+
+	-- pixel-by-pixel processing
+	local colTrans = imDiff:colorAllocateAlpha(255, 255, 255, 127)
+	for y = 0, height - 1 do
+		for x = 0, width - 1 do
+			local colFG = imFG:getPixel(x + xOffsetFG, y + yOffsetFG)
+			local colBG = imBG:getPixel(x + xOffsetBG, y + yOffsetBG)
+			if imFG:red(colFG) == imBG:red(colBG) and
+			   imFG:green(colFG) == imBG:green(colBG) and
+			   imFG:blue(colFG) == imBG:blue(colBG)
+			then
+				imDiff:setPixel(x, y, colTrans)
+			end
+		end
+	end
+	imDiff:alphaBlending(true)
+	imDiff:saveAlpha(false)
+	return imDiff
+end
+
+gdJoL = gd.convertToTrueColor(gd.createFromPng(root..jogf))
+gdJoR = gd.flipHorizontal(gd.convertToTrueColor(gd.createFromPng(root..jogf)))
+gdChL = gd.convertToTrueColor(gd.createFromPng(root..chgf))
+gdChR = gd.flipHorizontal(gd.convertToTrueColor(gd.createFromPng(root..chgf)))
+gdJoL:alphaBlending(true)
+gdJoR:alphaBlending(true)
+gdChL:alphaBlending(true)
+gdChR:alphaBlending(true)
+gdJoL:saveAlpha(true)
+gdJoR:saveAlpha(true)
+gdChL:saveAlpha(true)
+gdChR:saveAlpha(true)
+
+jogl = gdJoL:gdStr()
+jogr = gdJoR:gdStr()
+chgl = gdChL:gdStr()
+chgr = gdChR:gdStr()
 
 function joDrawSprite(x, y, n, reverse)
 	local xi, yi = (n % 0x10), math.floor(n / 0x10)
@@ -98,7 +166,10 @@ function chDrawSprite(x, y, n, reverse)
 	end
 end
 
+local input_saved = input.get()
+local image_save_lock = false
 gui.register(function()
+	local input_curr = input.get()
 	jo_visible = bit.band(memory.readbyte(0x020fcaf5), 0x80)==0
 	ch_visible = bit.band(memory.readbyte(0x020fcc55), 0x80)==0
 	room_x = memory.readbyte(0x02111778)
@@ -131,6 +202,16 @@ gui.register(function()
 	ch_hity1 = memory.readwordsigned(0x02132984)
 	ch_hitx2 = memory.readwordsigned(0x02132986)
 	ch_hity2 = memory.readwordsigned(0x02132988)
+	my_x = jo_x
+	my_y = jo_y
+	my_dir = jo_dir
+	my_spr = jo_spr
+	my_hitx1 = jo_hitx1
+	my_hity1 = jo_hity1
+	my_hitx2 = jo_hitx2
+	my_hity2 = jo_hity2
+	mygd = gdJoL
+	mygdFilename = jogf
 	gui.text(164, 0, string.format("cams: %d %d", camx, camy))
 	gui.text(164, 10, string.format("area: %d %d %d", area, room_x, room_y))
 	if jo_visible then
@@ -167,4 +248,27 @@ gui.register(function()
 		-- gui.box(ch_hitx1 - camx, ch_hity1 - camy, ch_hitx2 - camx, ch_hity2 - camy, "clear", "green")
 		gui.opacity(1 * fade)
 	end
+
+	if input_curr[screenshotKey] and not input_saved[screenshotKey] and not image_save_lock then
+		image_save_lock = true
+
+		local gdEmu = gd.createFromPngStr(gd.createFromGdStr(gui.gdscreenshot()):pngStr())
+		--gdEmu:png(root.."screenshot.png")
+		local gdBG = gd.createFromPng(root.."SpriteRipBG.png")
+		local x, y = my_x - camx - sprox, my_y - camy - sproy + 192
+		local gdDiff = gd.createDiff(gdEmu, gdBG, x, y, sprw, sprh)
+		local xOfsDB, yOfsDB = (my_spr%0x10) * sprw, math.floor(my_spr/0x10) * sprh
+		mygd:alphaBlending(false)
+		mygd:copy(gdDiff, xOfsDB, yOfsDB, 0, 0, sprw, sprh)
+		mygd:alphaBlending(true)
+		mygd:png(root..mygdFilename)
+		print(mygdFilename .. ": sprite #" .. my_spr .. " saved")
+
+		-- update current display
+		jogl = gdJoL:gdStr()
+
+		image_save_lock = false
+	end
+
+	input_saved = input_curr
 end)
